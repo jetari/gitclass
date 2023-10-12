addb06f8e38c5dbda38925af6beabc8574c57d04
jetari <epretarijonah@gmail.com>
2023-10-12 05:13:57 +0100

    Commiting all my Assignemnt files

---
+++ Trump/brian.js
@@ -0,0 +1,25 @@
+// let val = "Hello world";
+
+// console.log(val)
+
+// let a = 4
+// let b = 7
+// let c = b-a
+
+// console.log(c)
+
+let firstName = "joy"
+let stack = "node"
+const introduction = `Hi ${firstName} Welcome to decagon your stack is ${stack}`;
+console.log(introduction);
+
+
+// ~
+// `Hi firstName Welcome to decagon your stack is node`
+
+// console.log(${"Hi"} + $firstName + ${"Welcome to decagon your stack is"} + $stack")
+
+
+
+
+// console.log("Hi " + firstName + " Welcome to decagon your stack is " + stack)
--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
+// coded by @ChaituVR
+const projectName = 'tribute-page';
+localStorage.setItem('example_project', 'Tribute Page');
+
+
 
